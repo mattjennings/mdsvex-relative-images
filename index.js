c7b71118dac1fd5cd11b347d102d41a5eefdb40a
ThisIsFayad <thisisfayad@proton.me>
2025-03-05 15:42:25 -0600

    feat: update regex to match only valid paths and support aliases

--- index.js
+++ index.js
@@ -4,87 +4,85 @@ import { visit } from "unist-util-visit";
 import toCamel from "just-camel-case";
 
 const RE_SCRIPT_START =
-  /<script(?:\s+?[a-zA-z]+(=(?:["']){0,1}[a-zA-Z0-9]+(?:["']){0,1}){0,1})*\s*?>/;
-const RE_PROPS = /(\w+)\s*=\s*(["'])(.*?)\2/g;
+    /<script(?:\s+?[a-zA-z]+(=(?:["']){0,1}[a-zA-Z0-9]+(?:["']){0,1}){0,1})*\s*?>/;
+const RE_PROPS = /(\w+)\s*=\s*(["'])(\s*(\$\w+|\.{1,2})[\/\\].*?\.\w+\s*)\2/g;
+const RE_PATH = /\s*(\$\w+|\.{1,2})[\/\\].*?\.\w+\s*/;
 
 export default function relativeImages() {
-  return function transformer(tree) {
-    const urls = new Map();
-    const url_count = new Map();
+    return function transformer(tree) {
+        const urls = new Map();
+        const url_count = new Map();
 
-    function transformUrl(url) {
-      url = url.trim();
-      url = decodeURIComponent(url)
-      
-      if (url.startsWith("./") || url.startsWith(".\\")) {
-        // filenames can start with digits,
-        // prepend underscore to guarantee valid module name
-        let camel = `_${toCamel(url)}`;
-        const count = url_count.get(camel);
-        const dupe = urls.get(url);
+        function transformUrl(url) {
+            url = url.trim();
+            url = decodeURIComponent(url);
+            let match = url.match(RE_PATH);
 
-        if (count && !dupe) {
-          url_count.set(camel, count + 1);
-          camel = `${camel}_${count}`;
-        } else if (!dupe) {
-          url_count.set(camel, 1);
+            if (match && match.length && match[0] == url) {
+                // filenames can start with digits,
+                // prepend underscore to guarantee valid module name
+                let camel = `_${toCamel(url)}`;
+                const count = url_count.get(camel);
+                const dupe = urls.get(url);
+
+                if (count && !dupe) {
+                    url_count.set(camel, count + 1);
+                    camel = `${camel}_${count}`;
+                } else if (!dupe) {
+                    url_count.set(camel, 1);
+                }
+
+                urls.set(url, {
+                    path: url,
+                    id: camel,
+                });
+
+                return `{${camel}}`;
+            }
+
+            return url;
         }
 
-        urls.set(url, {
-          path: url,
-          id: camel,
+        // transform urls in images
+        visit(tree, ["image", "definition"], (node) => {
+            node.url = transformUrl(node.url);
         });
 
-        return `{${camel}}`;
-      }
+        // transform src in html nodes
+        visit(tree, "html", (node) => {
+            let match;
+            const props = [];
 
-      return url;
-    }
+            while ((match = RE_PROPS.exec(node.value)) !== null) {
+                if (match[1]) props.push(match[3]);
+            }
 
-    // transform urls in images
-    visit(tree, ["image", "definition"], (node) => {
-      node.url = transformUrl(node.url);
-    });
+            for (let i = 0; i < props.length; i++) {
+                let url = props[i];
+                const transformed = transformUrl(url);
+                node.value = node.value.replace(`${url}`, transformed);
+            }
+        });
 
-    // transform src in html nodes
-    visit(tree, "html", (node) => {
-      let match;
-      const props = [];
+        let scripts = "";
+        urls.forEach((x) => (scripts += `import ${x.id} from "${x.path}";\n`));
 
-      while ((match = RE_PROPS.exec(node.value)) !== null) {
-        if (match[1]) {
-          props.push(match[3]);
-        } else if (match[4]) {
-          props.push(match[5]);
+        let is_script = false;
+
+        visit(tree, "html", (node) => {
+            if (RE_SCRIPT_START.test(node.value)) {
+                is_script = true;
+                node.value = node.value.replace(RE_SCRIPT_START, (script) => {
+                    return `${script}\n${scripts}`;
+                });
+            }
+        });
+
+        if (!is_script) {
+            tree.children.push({
+                type: "html",
+                value: `<script>\n${scripts}</script>`,
+            });
         }
-      }
-
-      for (let i = 0; i < props.length; i++) {
-        let url = props[i];
-        const transformed = transformUrl(url);
-        node.value = node.value.replace(`${url}`, transformed);
-      }
-    });
-
-    let scripts = "";
-    urls.forEach((x) => (scripts += `import ${x.id} from "${x.path}";\n`));
-
-    let is_script = false;
-
-    visit(tree, "html", (node) => {
-      if (RE_SCRIPT_START.test(node.value)) {
-        is_script = true;
-        node.value = node.value.replace(RE_SCRIPT_START, (script) => {
-          return `${script}\n${scripts}`;
-        });
-      }
-    });
-
-    if (!is_script) {
-      tree.children.push({
-        type: "html",
-        value: `<script>\n${scripts}</script>`,
-      });
-    }
-  };
+    };
 }
